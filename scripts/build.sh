cf43b78e49118cbbace2f4086bc8c263cb423d5f
Thijs Brobbel <thijsbrobbel@gmail.com>
2013-11-06 17:51:03 +0100

    Makes build scripts show build output.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,7 @@ BUILD_TARGET=$1
 
 CAMBRIDGE_PRODUCT=/opt/smallworld/cambridge_db
 export SW_WHICH_GIS_ALIAS_FILE=${CAMBRIDGE_PRODUCT}/config/magik_images/resources/base/data/gis_aliases
+export LOG_DIR=/dev
 
 BASE_DIR=$(dirname $0)
 BUILD_DIR=${BASE_DIR}/build
@@ -11,4 +12,9 @@ BUILD_DIR=${BASE_DIR}/build
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
-gis $BUILD_TARGET
+(until killall fgrep
+do
+  sleep 1
+done) >/dev/null 2>&1 &
+
+gis $BUILD_TARGET stdout
