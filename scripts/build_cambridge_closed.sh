17a266013a60ff29c8e387d57cac4edd8b429d8b
Thijs Brobbel <thijsbrobbel@gmail.com>
2013-11-06 15:58:19 +0100

    Adds build scripts for cambridge closed.

---
+++ scripts/build_cambridge_closed.sh
@@ -0,0 +1,1 @@
+gis -i -a /opt/smallworld/cambridge_db/config/magik_images/resources/base/data/gis_aliases build_cam_db_closed
---
+++ scripts/invoke-build_cambridge_closed.sh
@@ -0,0 +1,1 @@
+vagrant ssh -c '/vagrant/scripts/build_cambridge_closed.sh' -- -X
