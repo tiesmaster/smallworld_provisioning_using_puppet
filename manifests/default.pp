a672f3b31e4be91b7b5161aaed5df11e8e344e93
Thijs Brobbel <thijsbrobbel@gmail.com>
2013-11-06 15:19:26 +0100

    Renames install_dir -> target_dir.

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,6 +1,6 @@
 $installation_mount = "/mnt/smallworld_install"
 $installation_iso_file = "/vagrant/install/CORE420_UNIX.iso"
-$install_dir = "/opt/smallworld"
+$target_dir = "/opt/smallworld"
 $install_opts = "-force_os_rev -platforms local -emacs no -owner vagrant"
 
 file { "/bin/arch":
@@ -42,9 +42,9 @@ mount { "$installation_mount":
 }
 
 exec { "install smallworld":
-  command  => "$installation_mount/product/install.sh $install_opts -targetdir $install_dir -nolog < /vagrant/manifests/smallworld_install.answer_file",
+  command  => "$installation_mount/product/install.sh $install_opts -targetdir $target_dir -nolog < /vagrant/manifests/smallworld_install.answer_file",
   provider => shell,
-  creates  => "$install_dir",
+  creates  => "$target_dir",
   require => [
     File["/bin/arch"],
     Package["csh"],
@@ -53,20 +53,20 @@ exec { "install smallworld":
 }
 
 exec { "configure smallworld":
-  command   => "echo vagrant | $install_dir/GIS42/bin/share/gis_config -user",
+  command   => "echo vagrant | $target_dir/GIS42/bin/share/gis_config -user",
   provider  => shell,
   require   => Exec["install smallworld"],
 }
 
 exec { "patches font file":
-  command  => "sed -i -e 's/urw/*/' -e 's/0/*/' $install_dir/GIS42/config/font/urw_helvetica",
+  command  => "sed -i -e 's/urw/*/' -e 's/0/*/' $target_dir/GIS42/config/font/urw_helvetica",
   provider => shell,
   require  => Exec["install smallworld"],
 }
 
-file { "$install_dir/GIS42/config/gis_aliases":
+file { "$target_dir/GIS42/config/gis_aliases":
   ensure  => link,
-  target  => "$install_dir/GIS42/config/magik_images/resources/base/data/gis_aliases",
+  target  => "$target_dir/GIS42/config/magik_images/resources/base/data/gis_aliases",
   require => Exec["install smallworld"],
 }
 
@@ -74,7 +74,7 @@ exec { "test smallworld":
   command   => "sudo -H -u vagrant sh -l -c 'gis -i gis'",
   provider  => shell,
   logoutput => true,
-  require   => File["$install_dir/GIS42/config/gis_aliases"],
+  require   => File["$target_dir/GIS42/config/gis_aliases"],
 }
 
 # gis -i -a /opt/smallworld/cambridge_db/config/magik_images/resources/base/data/gis_aliases build_cam_db_closed
