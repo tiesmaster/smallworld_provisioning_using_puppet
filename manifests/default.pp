9616d7b1b884ca13c608068d05b176d5e361c89b
Thijs Brobbel <thijsbrobbel@gmail.com>
2013-11-06 15:39:22 +0100

    Documents logical parts in puppet file.

--- manifests/default.pp
+++ manifests/default.pp
@@ -4,6 +4,22 @@ $installation_iso_file = "/vagrant/install/CORE420_UNIX.iso"
 $target_dir = "/opt/smallworld"
 $target_user = "vagrant"
 
+# setup install location
+
+file { "${install_source}":
+  ensure => directory,
+}
+
+mount { "${install_source}":
+  ensure  => mounted,
+  device  => "${installation_iso_file}",
+  options => "loop,ro,noauto",
+  fstype  => "iso9660",
+  require => File["${install_source}"],
+}
+
+# installation deps
+
 file { "/bin/arch":
   ensure => link,
   target => "/usr/bin/arch",
@@ -12,6 +28,40 @@ file { "/bin/arch":
 package { 'csh':
   ensure => 'installed',
 }
+
+# install smallworld
+
+$install_dir = "-targetdir ${target_dir}"
+$install_features = "-force_os_rev -platforms local -emacs no -owner ${target_user}"
+$install_answer_file = "/vagrant/manifests/smallworld_install.answer_file"
+$install_opts = "${install_features} -nolog ${install_dir} < ${install_answer_file}"
+
+exec { "install smallworld":
+  command  => "${install_source}/product/install.sh ${install_opts}",
+  provider => shell,
+  creates  => "${target_dir}",
+  require => [
+    File["/bin/arch"],
+    Package["csh"],
+    Mount["${install_source}"],
+  ],
+}
+
+# configure smallworld
+
+exec { "configure smallworld":
+  command   => "echo ${target_user} | ${target_dir}/GIS42/bin/share/gis_config -user",
+  provider  => shell,
+  require   => Exec["install smallworld"],
+}
+
+file { "${target_dir}/GIS42/config/gis_aliases":
+  ensure  => link,
+  target  => "${target_dir}/GIS42/config/magik_images/resources/base/data/gis_aliases",
+  require => Exec["install smallworld"],
+}
+
+# runtime deps: sw_magik_motif
 
 package { 'libxaw3dxft6':
   ensure => 'installed',
@@ -30,51 +80,13 @@ file { '/usr/lib/libXaw3d.so.7':
   target => '/usr/lib/libXaw3dxft.so.6',
 }
 
-file { "${install_source}":
-  ensure => directory,
-}
-
-mount { "${install_source}":
-  ensure  => mounted,
-  device  => "${installation_iso_file}",
-  options => "loop,ro,noauto",
-  fstype  => "iso9660",
-  require => File["${install_source}"],
-}
-
-$install_dir = "-targetdir ${target_dir}"
-$install_features = "-force_os_rev -platforms local -emacs no -owner ${target_user}"
-$install_answer_file = "/vagrant/manifests/smallworld_install.answer_file"
-$install_opts = "${install_features} -nolog ${install_dir} < ${install_answer_file}"
-
-exec { "install smallworld":
-  command  => "${install_source}/product/install.sh ${install_opts}",
-  provider => shell,
-  creates  => "${target_dir}",
-  require => [
-    File["/bin/arch"],
-    Package["csh"],
-    Mount["${install_source}"],
-  ],
-}
-
-exec { "configure smallworld":
-  command   => "echo ${target_user} | ${target_dir}/GIS42/bin/share/gis_config -user",
-  provider  => shell,
-  require   => Exec["install smallworld"],
-}
-
 exec { "patches font file":
   command  => "sed -i -e 's/urw/*/' -e 's/0/*/' ${target_dir}/GIS42/config/font/urw_helvetica",
   provider => shell,
   require  => Exec["install smallworld"],
 }
 
-file { "${target_dir}/GIS42/config/gis_aliases":
-  ensure  => link,
-  target  => "${target_dir}/GIS42/config/magik_images/resources/base/data/gis_aliases",
-  require => Exec["install smallworld"],
-}
+# tests
 
 exec { "test smallworld":
   command   => "sudo -H -u ${target_user} sh -l -c 'gis -i gis'",
