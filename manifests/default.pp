28997394ce7d7c66b71c3fe200c91ed795c43798
Thijs Brobbel <thijsbrobbel@gmail.com>
2013-11-06 13:25:09 +0100

    Adds installing cambridge db product.

--- manifests/default.pp
+++ manifests/default.pp
@@ -76,3 +76,6 @@ exec { "test smallworld":
   logoutput => true,
   require   => File["$install_dir/GIS42/config/gis_aliases"],
 }
+
+# gis -i -a /opt/smallworld/cambridge_db/config/magik_images/resources/base/data/gis_aliases build_cam_db_closed
+# gis -i -a /opt/smallworld/cambridge_db/config/magik_images/resources/base/data/gis_aliases build_cam_db_closed_swaf
